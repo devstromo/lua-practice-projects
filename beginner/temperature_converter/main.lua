64a50cb35a08912e6177cc94e467670e8e902cfc
alemoles <alemolerio@gmail.com>
2025-03-11 22:07:10 -0300

    Format temp output

--- beginner/temperature_converter/main.lua
+++ beginner/temperature_converter/main.lua
@@ -1,4 +1,5 @@
 io.write("Convert F into Celsius: ")
 local f = io.read("*number")
 local c = (f - 32) * 5 / 9
-print("Celsius: " .. c)
+local formatted_result = string.format("%.4f", c)
+print("Celsius: " .. formatted_result)
