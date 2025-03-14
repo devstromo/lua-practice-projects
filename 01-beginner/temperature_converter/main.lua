9b1ab85539406abc3cadfe5cfb232d43f3733c5d
alemoles <alemolerio@gmail.com>
2025-03-13 22:24:52 -0300

    add convert Fahrenheit to Kelvin option

--- 01-beginner/temperature_converter/main.lua
+++ 01-beginner/temperature_converter/main.lua
@@ -5,6 +5,7 @@ Choose a convert option number
     .. "3. Convert K into C\n"
     .. "4. Convert C into K\n"
     .. "5. Convert K into F\n"
+    .. "6. Convert F into K\n"
     .. "Enter your option: "
 io.write(input)
 local option = io.read("*number")
@@ -38,6 +39,12 @@ elseif option == 5 then
     local f = (k - 273.15) * 9 / 5 + 32
     local formatted_result = string.format("%.4f", f)
     print("Fahrenheit: " .. formatted_result)
+elseif option == 6 then
+    io.write("Enter Fahrenheit: ")
+    local f = io.read("*number")
+    local k = ((f - 32) * 5 / 9) + 273.15
+    local formatted_result = string.format("%.4f", k)
+    print("Kelvin: " .. formatted_result)
 else
     print("Invalid option")
 end
