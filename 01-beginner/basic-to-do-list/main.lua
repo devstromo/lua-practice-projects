05a2adad7f8e0fe4d20087009a8c5ae838e9909c
alemoles <alemolerio@gmail.com>
2025-03-16 18:23:05 -0300

    Fix task input

--- 01-beginner/basic-to-do-list/main.lua
+++ 01-beginner/basic-to-do-list/main.lua
@@ -4,12 +4,16 @@ Welcome back to the basic to do list, what would you like to do?
     .. "2. List my tasks\n"
 io.write(input)
 local option = io.read("*number")
+io.read()
 if option == 1 then
     io.write("Enter your task: ")
     local task = io.read()
+    io.write("You entered: " .. task .. "\n")
+    -- Save task to file
     local file = io.open("tasks.txt", "a")
     file:write(task .. "\n")
     file:close()
+
     print("Task added successfully")
 elseif option == 2 then
     local file = io.open("tasks.txt", "r")
