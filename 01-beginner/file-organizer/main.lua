553d1a178f8990df7d9df27137b4fc4c651e7967
alemoles <alemolerio@gmail.com>
2025-03-21 23:21:32 -0300

    validate source and destination path

--- 01-beginner/file-organizer/main.lua
+++ 01-beginner/file-organizer/main.lua
@@ -1,3 +1,13 @@
+local function isValidPath(path)
+    local file = io.open(path, "r")
+    if file then
+        file:close()
+        return true
+    else
+        return false
+    end
+end
+
 -- MAIN
 local input = [[
 Welcome to the basic file organizer, what would you like to do?
@@ -16,7 +26,19 @@ if option == 1 then
     io.write("Enter the destination path: ")
     local destinationPath = io.input():read()
     print("Source path: " .. sourcePath)
+    if isValidPath(sourcePath) then
+        print("Source path is valid.")
+    else
+        print("Source path is invalid.")
+        return
+    end
     print("Destination path: " .. destinationPath)
+    if isValidPath(destinationPath) then
+        print("Destination path is valid.")
+    else
+        print("Destination path is invalid.")
+        return
+    end
 elseif option == 2 then
     print("Help")
 elseif option == 3 then
