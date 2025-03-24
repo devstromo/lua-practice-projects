1ce1432842d68e4d3d1e54fde0bc32d5eb4c4676
alemoles <alemolerio@gmail.com>
2025-03-23 21:09:50 -0300

    improve menu input options

--- 01-beginner/file-organizer/main.lua
+++ 01-beginner/file-organizer/main.lua
@@ -38,44 +38,46 @@ Welcome to the basic file organizer, what would you like to do?
 2. Help
 3. Exit
 ]]
-io.write(input)
+repeat
+    io.write(input)
 
-local option = io.read("*number")
-local _ = io.read() -- Consume newline
-if option == 1 then
-    print("Organize files")
-    io.write("Enter the source path: ")
-    local sourcePath = io.input():read()
-    io.write("Enter the destination path: ")
-    local destinationPath = io.input():read()
-    print("Source path: " .. sourcePath)
-    if isValidPath(sourcePath) then
-        print("Source path is valid.")
+    local option = io.read("*number")
+    local _ = io.read() -- Consume newline
+    if option == 1 then
+        print("Organize files")
+        io.write("Enter the source path: ")
+        local sourcePath = io.input():read()
+        io.write("Enter the destination path: ")
+        local destinationPath = io.input():read()
+        print("Source path: " .. sourcePath)
+        if isValidPath(sourcePath) then
+            print("Source path is valid.")
+        else
+            print("Source path is invalid.")
+            return
+        end
+        print("Destination path: " .. destinationPath)
+        if isValidPath(destinationPath) then
+            print("Destination path is valid.")
+        else
+            print("Destination path is invalid.")
+            return
+        end
+        local folder_path = "."
+        local file_type = "txt"
+        local max_depth = 2
+
+        local files = scan_folder(sourcePath, file_type, max_depth)
+
+        print("Found " .. #files .. " ." .. file_type .. " files:")
+        for _, file in ipairs(files) do
+            print(file)
+        end
+    elseif option == 2 then
+        print("Help\n")
+    elseif option == 3 then
+        print("Exit\n")
     else
-        print("Source path is invalid.")
-        return
+        print("Invalid option\n")
     end
-    print("Destination path: " .. destinationPath)
-    if isValidPath(destinationPath) then
-        print("Destination path is valid.")
-    else
-        print("Destination path is invalid.")
-        return
-    end
-    local folder_path = "."
-    local file_type = "txt"
-    local max_depth = 2
-
-    local files = scan_folder(sourcePath, file_type, max_depth)
-
-    print("Found " .. #files .. " ." .. file_type .. " files:")
-    for _, file in ipairs(files) do
-        print(file)
-    end
-elseif option == 2 then
-    print("Help")
-elseif option == 3 then
-    print("Exit")
-else
-    print("Invalid option")
-end
+until option == 3
