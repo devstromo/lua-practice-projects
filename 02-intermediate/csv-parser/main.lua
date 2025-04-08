7d845f0e3dc4e97d3c282c2dd5d692e3a84b412a
alemoles <alemolerio@gmail.com>
2025-04-07 22:48:21 -0300

    create example csv parser

---
+++ 02-intermediate/csv-parser/main.lua
@@ -0,0 +1,42 @@
+
+
+-- MAIN
+local function parse_csv_line(line)
+    local fields = {}
+    local field = ""
+    local inside_quotes = false
+
+    for i = 1, #line do
+        local c = line:sub(i, i)
+        if c == '"' then
+            inside_quotes = not inside_quotes
+        elseif c == ',' and not inside_quotes then
+            table.insert(fields, field)
+            field = ""
+        else
+            field = field .. c
+        end
+    end
+
+    table.insert(fields, field) -- last field
+    return fields
+end
+
+local filename = "flat-ui__data-us.csv"
+local file = io.open(filename, "r")
+
+if not file then
+    print("File not found!")
+    os.exit(1)
+end
+
+for line in file:lines() do
+    local row = parse_csv_line(line)
+    for i, value in ipairs(row) do
+        print(string.format("Column %d: %s", i, value))
+    end
+    print("------")
+end
+
+file:close()
+
