81c1cf1b38cce729f58128b7f9e8164ce8b1992f
alemoles <alemolerio@gmail.com>
2025-05-19 22:56:28 -0300

    Refactor status code reporting to track and display the most common status code

--- 02-intermediate/log-analizer/analyzer/plugins/status_codes.lua
+++ 02-intermediate/log-analizer/analyzer/plugins/status_codes.lua
@@ -11,8 +11,19 @@ end
 
 function M.report()
     print("\nStatus code counts:")
+    local max = 0
+    local max_code = ""
     for code, count in pairs(codes) do
         print("  " .. code .. ": " .. count)
+        if count > max then
+            max = count
+            max_code = code
+        end
+    end
+    if max > 0 then
+        print("Most common status code: " .. max_code .. " (" .. max .. " occurrences)")
+    else
+        print("No status codes found.")
     end
 end
 
