848577c418acc6b287832222b13dbccbf3748196
alemoles <alemolerio@gmail.com>
2025-05-26 23:59:01 -0300

    Refactor timestamp parsing in requests_by_time plugin to support multiple formats and improve bucket extraction logic

--- 02-intermediate/log-analizer/analyzer/plugins/requests_by_time.lua
+++ 02-intermediate/log-analizer/analyzer/plugins/requests_by_time.lua
@@ -1,7 +1,7 @@
 -- analyzer/plugins/requests_by_time.lua
 local M = {}
 local time_buckets = {}
-M.granularity = "minute"  -- can be: "hour", "minute", "second"
+M.granularity = "minute" -- can be: "hour", "minute", "second"
 
 -- Allow CLI arg like --granularity=second
 function M.set_args(args)
@@ -10,34 +10,90 @@ function M.set_args(args)
     end
 end
 
-local function extract_bucket(datetime_str, granularity)
-    -- Expected input: 12/May/2025:21:14:59 (Apache-style)
-    local d, m, y, h, min, s = datetime_str:match("(%d%d)/(%a+)/(%d%d%d%d):(%d%d):(%d%d):(%d%d)")
-    if not (d and m and y and h and min and s) then return nil end
-
-    local month_map = {
-        Jan = "01", Feb = "02", Mar = "03", Apr = "04", May = "05", Jun = "06",
-        Jul = "07", Aug = "08", Sep = "09", Oct = "10", Nov = "11", Dec = "12"
-    }
-
-    m = month_map[m]
-    if not m then return nil end
-
-    if granularity == "hour" then
-        return string.format("%s-%s-%s %s:00", y, m, d, h)
-    elseif granularity == "minute" then
-        return string.format("%s-%s-%s %s:%s", y, m, d, h, min)
-    elseif granularity == "second" then
-        return string.format("%s-%s-%s %s:%s:%s", y, m, d, h, min, s)
+local function parse_timestamp(line)
+    -- Try ISO format: 2015-07-31 00:16:12,152
+    local y, m, d, h, min, s = line:match("(%d%d%d%d)%-(%d%d)%-(%d%d) (%d%d):(%d%d):(%d%d)")
+    if y then
+        return {
+            year = y,
+            month = m,
+            day = d,
+            hour = h,
+            min = min,
+            sec = s
+        }
     end
+
+    -- Try Apache: [Mon Dec 05 13:41:58 2005]
+    local mon, day, h, min, s, y = line:match("%[%a+ (%a+) (%d+) (%d+):(%d+):(%d+) (%d+)%]")
+    if mon then
+        local month_map = {
+            Jan = 1,
+            Feb = 2,
+            Mar = 3,
+            Apr = 4,
+            May = 5,
+            Jun = 6,
+            Jul = 7,
+            Aug = 8,
+            Sep = 9,
+            Oct = 10,
+            Nov = 11,
+            Dec = 12
+        }
+        return {
+            year = y,
+            month = month_map[mon],
+            day = day,
+            hour = h,
+            min = min,
+            sec = s
+        }
+    end
+
+    -- Try Android format: 03-17 16:13:47.631
+    local mon, day, h, min, s = line:match("(%d%d)%-(%d%d) (%d%d):(%d%d):(%d%d)")
+    if mon then
+        return {
+            year = os.date("*t").year, -- assume current year
+            month = mon,
+            day = day,
+            hour = h,
+            min = min,
+            sec = s
+        }
+    end
+
+    return nil
 end
 
 function M.process_line(line)
-    local timestamp = line:match("%[(.-)%]")  -- Match date in square brackets
-    if not timestamp then return end
+    local t = parse_timestamp(line)
+    if not t then
+        return
+    end
 
-    local bucket = extract_bucket(timestamp, M.granularity)
-    if not bucket then return end
+    local timestamp = os.time({
+        year = tonumber(t.year),
+        month = tonumber(t.month),
+        day = tonumber(t.day),
+        hour = tonumber(t.hour),
+        min = tonumber(t.min),
+        sec = tonumber(t.sec)
+    })
+
+    if not timestamp then
+        return
+    end
+
+    local bucket
+    if M.granularity == "hour" then
+        bucket = os.date("%Y-%m-%d %H:00", timestamp)
+    elseif M.granularity == "minute" then
+        bucket = os.date("%Y-%m-%d %H:%M", timestamp)
+    elseif M.granularity == "second" then
+        bucket = os.date("%Y-%m-%d %H:%M:%S", timestamp)
+    end
 
     time_buckets[bucket] = (time_buckets[bucket] or 0) + 1
 end
